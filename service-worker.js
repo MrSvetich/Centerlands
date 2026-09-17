56a5866a3fe0e05d7a6b6806e477b8ab8989a8c7
MrSvetich <mr.svetich@gmail.com>
2026-09-17 15:18:15 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='centerlands-v010';
+const CACHE='centerlands-v011';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
