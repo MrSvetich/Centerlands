5a2852be412bf3f1e99551a6696d763c3c5b9202
MrSvetich <mr.svetich@gmail.com>
2026-09-17 16:15:14 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,14 @@
-const CACHE='centerlands-v014';
-const ASSETS=['./','./index.html','./manifest.json','./home_island_v014.jpg','./worker_1.png','./worker_2.png','./worker_3.png'];
+const CACHE='centerlands-v015';
+const ASSETS=['./','./index.html','./manifest.json','./home_island_v015.png','./worker_1.png','./worker_2.png','./worker_3.png'];
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+ self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+ self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+ if(e.request.method!=='GET') return;
+ e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
